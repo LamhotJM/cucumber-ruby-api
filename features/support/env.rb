73fc4b12d63e45d67ee30aac88bb42ae6820d4f1
lamhotjm <lamhotjms@gmail.com>
2018-07-22 22:07:03 +0700

    beautify

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,14 +4,12 @@ require 'dotenv'
 Dotenv.load
 
 Before do
-  @USER_AGENT = ENV['USER_AGENT']
-  @TIMEOUT = 480
-  @BASE_URL = ENV['BASE_URL'] 
-  puts "Host: #{@BASE_URL}, user agent: #{@USER_AGENT}"
+  puts "Start Cucumber Automated API Testing"
 end
 
 After do |scenario|
   if scenario.failed?
     puts scenario
   end
+  puts "End Cucumber Automated API Testing"
 end
